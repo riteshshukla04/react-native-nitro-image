22bc7e55b6afee7d3f09cdab110f5e633bd49e55
Ritesh Shukla <riteshshukla2381@gmail.com>
2026-04-07 11:56:26 +0530

    Update NitroWebImagePackage.java

--- packages/react-native-nitro-web-image/android/src/main/java/com/margelo/nitro/web/image/NitroWebImagePackage.java
+++ packages/react-native-nitro-web-image/android/src/main/java/com/margelo/nitro/web/image/NitroWebImagePackage.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
-import com.facebook.react.TurboReactPackage;
+import com.facebook.react.BaseReactPackage;
 import com.facebook.react.uimanager.ViewManager;
 import com.margelo.nitro.core.HybridObject;
 
@@ -17,7 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.function.Supplier;
 
-public class NitroWebImagePackage extends TurboReactPackage {
+public class NitroWebImagePackage extends BaseReactPackage {
   @Nullable
   @Override
   public NativeModule getModule(String name, ReactApplicationContext reactContext) {
